3f29714094d44f9622c9a29c5721a1f71d5c5673
benoit <rousseaux.benoit@yahoo.fr>
2020-02-11 15:56:20 +0100

    hiddent

--- js/library.js
+++ js/library.js
@@ -1,3 +1,8 @@
 function sumbitButtonOnClick(){
     alert("message envoyé");
 }
+
+function hideButton(el){
+    //document.getElementById(submitButton).style.display = none;
+    el.style.visibility='hidden';
+}
